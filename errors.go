1d7442f97999e24692af63a69afdc77c20cb1da3
hit9 <hit9@icloud.com>
2015-09-28 16:18:45 +0800

    Minor fix on error message

--- errors.go
+++ errors.go
@@ -3,7 +3,7 @@ package main
 import "errors"
 
 var (
-	ErrInvalidInput     = errors.New("invalid input")
-	ErrInvalidDBVal     = errors.New("invalid value in db")
-	ErrInvalidCfgFactor = errors.New("invalid factor in config")
+	ErrInvalidInput     = errors.New("invalid input from pub end")
+	ErrInvalidDBVal     = errors.New("invalid value is found in db")
+	ErrInvalidCfgFactor = errors.New("invalid factor in config (require 0~1)")
 )
