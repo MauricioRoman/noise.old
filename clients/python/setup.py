8060de583369a0e24c2db5ff746bc865500a5529
hit9 <hit9@icloud.com>
2015-10-10 13:39:20 +0800

    Bump python client 0.0.3

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(
     name="noise.py",
-    version="0.0.2",
+    version="0.0.3",
     author="hit9",
     author_email="hit9@ele.me",
     description="Python client for github.com/eleme/noise",
     license="MIT",
     keywords="noise, client",
-    url="https://github.com/eleme/noise/clients/python",
+    url="https://github.com/eleme/noise/tree/master/clients/python",
     py_modules=["noise"],
 )
