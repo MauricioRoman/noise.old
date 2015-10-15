088368cd05b6f7a30c024b66705a3e3b01811a5c
hit9 <hit9@icloud.com>
2015-10-15 17:46:20 +0800

    Add main cli

--- main.go
+++ main.go
@@ -11,10 +11,13 @@ import (
 )
 
 func main() {
-	fileName := flag.String("c", "config.json", "config path")
-	version := flag.Bool("v", false, "show version")
+	detector := flag.Bool("detector", false, "start detector")
+	// webapp := flag.Bool("webapp", false, "start webapp")
+	// alerter := flag.Bool("alerter", false, "start alerter")
+	fileName := flag.String("config", "", "config path")
+	version := flag.Bool("verion", false, "show version")
 	flag.Parse()
-	if flag.NArg() > 0 && flag.NFlag() != 1 {
+	if flag.NFlag() != 2 {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
@@ -26,7 +29,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to read %s: %v", *fileName, err)
 	}
-	StartDetector(cfg.Detector)
+	switch {
+	case *detector:
+		StartDetector(cfg.Detector)
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 }
 
 func StartDetector(cfg *config.SectionDetector) {
